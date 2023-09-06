1a19cc5c4993139329b6d5b5d6e99888f0c72ea7
Alinerml <1161606597@qq.com>
2023-09-06 17:49:22 +0800

    Changes 修改sql

--- sql/minigo.sql
+++ sql/minigo.sql
@@ -3,64 +3,139 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80033
+ Source Server Version : 80032
  Source Host           : localhost:3306
  Source Schema         : minigo
 
  Target Server Type    : MySQL
- Target Server Version : 80033
+ Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 08/08/2023 17:07:25
+ Date: 06/09/2023 17:48:45
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` int(0) NULL DEFAULT NULL,
+  `to_user_id` int(0) NULL DEFAULT NULL,
+  `comment_text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `comment_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `video_id` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (8, 15, 0, '好看', '9-6', 31);
+
+-- ----------------------------
+-- Table structure for follow
+-- ----------------------------
+DROP TABLE IF EXISTS `follow`;
+CREATE TABLE `follow`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `follower_id` int(0) NOT NULL COMMENT '关注人',
+  `follow_id` int(0) NOT NULL COMMENT '被关注人',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of follow
+-- ----------------------------
+INSERT INTO `follow` VALUES (13, 16, 15, '1693971939');
+INSERT INTO `follow` VALUES (15, 15, 16, '1693972091');
+
+-- ----------------------------
+-- Table structure for like
+-- ----------------------------
+DROP TABLE IF EXISTS `like`;
+CREATE TABLE `like`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` int(0) NOT NULL,
+  `video_id` int(0) NOT NULL,
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of like
+-- ----------------------------
+INSERT INTO `like` VALUES (23, 16, 30, '1693971957');
+INSERT INTO `like` VALUES (24, 15, 31, '1693972057');
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `to_user_id` int(0) NULL DEFAULT NULL,
+  `from_user_id` int(0) NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of message
+-- ----------------------------
+INSERT INTO `message` VALUES (8, 16, 15, '我爱你', '1693972112');
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(0) NOT NULL,
-  `user_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
-  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `id` int(0) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `create_time` bigint(0) NOT NULL COMMENT '创建时间',
   `follow_count` int(0) NULL DEFAULT NULL COMMENT '关注数量',
   `follower_count` int(0) NULL DEFAULT NULL COMMENT '粉丝数量',
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户头像',
   `background_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户个人页顶部大图',
   `signature` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '个人简介',
-  `total_favorited` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '获赞数量',
-  `work_count` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '作品数量',
-  `favorite_count` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '喜欢数量',
+  `total_favorited` int(0) NULL DEFAULT NULL COMMENT '获赞数量',
+  `work_count` int(0) NULL DEFAULT NULL COMMENT '作品数量',
+  `favorite_count` int(0) NULL DEFAULT NULL COMMENT '喜欢数量',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES (15, 'ml', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 1693971322, 1, 1, 'http://s0ce3vuua.bkt.clouddn.com/photo/20230905140635.jpg', 'http://s0ce3vuua.bkt.clouddn.com/photo/20230905140059.jpg', '这是一个固定的个性签名', 0, 1, 1);
+INSERT INTO `user` VALUES (16, 'yyj', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 1693971581, 1, 1, 'http://s0ce3vuua.bkt.clouddn.com/photo/20230905140635.jpg', 'http://s0ce3vuua.bkt.clouddn.com/photo/20230905140059.jpg', '这是一个固定的个性签名', 0, 1, 1);
 
 -- ----------------------------
 -- Table structure for video
 -- ----------------------------
 DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video`  (
-  `id` int(0) NOT NULL COMMENT 'id',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `play_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '视频播放地址',
   `cover_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '视频封面地址',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
+  `create_time` bigint(0) NOT NULL COMMENT '创建时间',
   `auth_id` int(0) NOT NULL COMMENT '作者id',
-  `auth_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '作者用户名',
   `favorite_count` int(0) NULL DEFAULT NULL COMMENT '视频点赞数量',
   `comment_count` int(0) NULL DEFAULT NULL COMMENT '视频评论数量',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '视频标题',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of video
 -- ----------------------------
+INSERT INTO `video` VALUES (30, 'http://s0ce3vuua.bkt.clouddn.com/video/15_1693971403_VIDEO_20230905_233150127.mp4', 'http://s0ce3vuua.bkt.clouddn.com/photo/15_1693971403_VIDEO_20230905_233150127.jpg', 1693971404, 15, 1, 0, '爱你爱你');
+INSERT INTO `video` VALUES (31, 'http://s0ce3vuua.bkt.clouddn.com/video/16_1693971983_0f0637658b993d60487945d7b20638af.mp4', 'http://s0ce3vuua.bkt.clouddn.com/photo/16_1693971983_0f0637658b993d60487945d7b20638af.jpg', 1693971985, 16, 1, 1, 'aaa');
 
 SET FOREIGN_KEY_CHECKS = 1;
