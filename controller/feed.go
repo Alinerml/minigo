8125a31f3e06e597fc7fe0ebe0ed354eabab119a
Alinerml <1161606597@qq.com>
2023-09-07 16:40:30 +0800

    fix:修改token失效bug

--- controller/feed.go
+++ controller/feed.go
@@ -24,7 +24,46 @@ func Feed(c *gin.Context) { //首页查询视频
 		token_p, _ := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 			return conf.SecretKey, nil
 		})
-		claims, _ := token_p.Claims.(jwt.MapClaims)
+		claims, ok := token_p.Claims.(jwt.MapClaims)
+		if !ok {
+			latest_time, _ := strconv.ParseInt(c.Query("latest_time"), 10, 64)
+			if latest_time == 0 {
+				latest_time = time.Now().Unix()
+			}
+			video_list := dao.QueryVideosByTime(latest_time)
+			video_res := make([]Video, len(video_list))
+
+			for i, source := range video_list {
+				user := dao.QueryUserById(source.AuthId)
+				video_res[i] = Video{
+					Id: source.Id,
+					Author: User{
+						Id:              user.Id,
+						Name:            user.Name,
+						FollowCount:     user.FollowCount,
+						FollowerCount:   user.FollowerCount,
+						IsFollow:        false,
+						Avatar:          user.Avatar,
+						BackgroundImage: user.BackgroundImage,
+						Signature:       user.Signature,
+						TotalFavorited:  user.TotalFavorited,
+						WorkCount:       user.WorkCount,
+						FavoriteCount:   user.FavoriteCount,
+					},
+					PlayUrl:       source.PlayUrl,
+					CoverUrl:      source.CoverUrl,
+					FavoriteCount: source.FavoriteCount,
+					CommentCount:  source.CommentCount,
+					IsFavorite:    false,
+					Title:         source.Title,
+				}
+			}
+			c.JSON(http.StatusOK, FeedResponse{
+				Response:  Response{StatusCode: 0},
+				VideoList: video_res,
+			})
+			return
+		}
 		user_id := claims["sub"] //得到用户id
 		authid := int64(user_id.(float64))
 		latest_time, _ := strconv.ParseInt(c.Query("latest_time"), 10, 64)
@@ -65,6 +104,7 @@ func Feed(c *gin.Context) { //首页查询视频
 			Response:  Response{StatusCode: 0},
 			VideoList: video_res,
 		})
+		return
 	} else {
 
 		latest_time, _ := strconv.ParseInt(c.Query("latest_time"), 10, 64)
@@ -103,6 +143,7 @@ func Feed(c *gin.Context) { //首页查询视频
 			Response:  Response{StatusCode: 0},
 			VideoList: video_res,
 		})
+		return
 	}
 
 }
